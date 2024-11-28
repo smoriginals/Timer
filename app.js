663c52945ef06957208caef35019d16fc52ea48e
SM ORIGINALS <SMOS-Github@users.noreply.github.com>
2024-11-28 22:53:28 +0530

    Update
    Can't Procced Right Now...
    Move to next Project This Project have minor bugs and update later.

--- app.js
+++ app.js
@@ -1,54 +1,40 @@
-////let countdown;
-////function Timer(e) {
-////    const now = Date.now();
-////    const then = now + e * 1000;
+﻿const ETG = document.querySelector('.etg');
+const ETA = document.querySelector('.eta');
+let countdown;
+function Timer(sec) {
+    if (countdown) clearInterval(countdown);
+    const now = Date.now();
+    const secs = sec * 1000;
+    const then = now + secs;
+    DisplayTimeLeft(sec);
+    DisplayEndTime(then);
 
-////    countdown = setInterval(() => {
-////        const secLeft = Math.round((then - Date.now()), 1000);
-////        if (secLeft <= 0) {
-////            clearInterval(countdown);
-////            return;
-////        }
-////        console.log(secLeft);
-////    }, 1000);
-////}
+    countdown = setInterval(() => {
+        const leftSec = Math.round((then - Date.now()) / 1000);
+        if (leftSec <0) {
+            clearInterval(countdown);
+            return;
+        }
+        DisplayTimeLeft(leftSec);
+    }, 1000);
 
-//let countdown;
-////const displayTime = document.querySelector(".eta");
-////const endTime = document.querySelector(".etg");
+}
 
-//function Timer(e) {
-//    const now = Date.now(); //get current time,day,date,sec,hour,min...etc.
-//    const then = now + e * 1000; // multiply by 1000 gives you miliseconds of that sec that you pass in Timer(sec).
+function DisplayTimeLeft(seconds) {
+    const min = Math.floor(seconds / 60);
+    const secRem = seconds % 60;
 
-//    //DisplayLeftTime(e);
-//    //DisplayEndTime(then)
-//    countdown = setInterval(() => {
-//        const secLeft = Math.round((then - Date.now()) / 1000);
-//        //check if we should stop it
-//        if (secLeft <= 0) {
-//            clearInterval(countdown);
-//            return;
-//        }
-//        //Display it
-//        DisplayLeftTime(e);
-//    }, 1000);
-//}
+    const display = `${min}:${secRem < 10 ? '0' : ''}${secRem}`;
+    //// ↑String↑////↑ 2min ↑ :↑        0sec     ↑__↑  5sec  ↑  --->  2:05 sec
+    ETG.innerHTML = display;
+}
 
-//function DisplayLeftTime(sec) {
-//    const min = Math.floor(sec / 60);
-//    const secLeft = sec % 60;
-//    const display = `${min}:${secLeft < 10 ? '0' : ''}${secLeft}`;
-//    document.title = display;
-//    displayTime.textContent = display;
+function DisplayEndTime(timeStamp) {
+    const end = new Date(timeStamp);
+    const hrs = end.getHours();
+    const min = end.getMinutes();
+    ETA.innerHTML = `Be back @ ${hrs < 10 ? '0' : 0}:${min < 10 ? '0' : ''}${min}`;
+   // ETA.innerHTML = `Be back @ ${hrs < 10 ? '0' : ''}${hrs}:${min < 10 ? '0' : ''}${min}`;
+}
 
-//}
-
-//function DisplayEndTime(timeStamp) {
-//    const end = new Date(timeStamp);
-//    const hour = end.getHours();
-//    const min = end.getMinutes();
-//    const adjustedHour = hour > 12 ? hour - 12 : '';
-//    endTime.textContent = `Be Back @${adjustedHour}:${min < 10 ? '0' : ''}${min}`;
-//}
-
+Timer(10);
